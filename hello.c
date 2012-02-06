811f7c132cf1137a83f02ad15e436459e17db14a
akaedu <akaedu@akaedu-desktop.(none)>
2012-02-06 16:55:36 +0800

    haha

--- hello.c
+++ hello.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+int a;
 float b;
 float sum;
 int main()
-{asdasdsadfasdfa
+{
+	scanf(a=%,b=%);
 	printf("Hello,word!\n");
 	printf("This is a C program!\n");
 	printf("哈哈，linux实在是太好玩啦！\n");
